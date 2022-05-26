7f18d61b66b5844f076d90a7538c0df869796448
giacomocastellan <giacomocastellan.assistant@gmail.com>
2022-05-26 22:46:40 +0200

    26/05

---
+++ esp_controller/esp_controller.ino
@@ -0,0 +1,50 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Linkem_FCD881_EXT";
+const char* password = "d29vust7";
+const char* mqttServer = "broker.emqx.io";
+const char* topic = "rotas.stage/rover";
+
+const int mqttPort = 1883;
+const char* mqttUser = "giaco";
+const char* mqttPassword = "giaco";
+
+WiFiClient espClient;
+PubSubClient client = PubSubClient(espClient);
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println("Connected to the WiFi network");
+
+  client.setServer(mqttServer, mqttPort);
+
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+
+    if (client.connect("ESP32Client", mqttUser, mqttPassword)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+
+}
+
+void loop() {
+  client.loop();
+
+  delay(1000);
+  client.publish(topic, "Hello");
+  Serial.println("hello");
+}
+
