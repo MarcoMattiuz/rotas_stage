5b77545933ae5c7bef554c2811a560485ce6a035
MarcoMattiuz <marccs35@gmail.com>
2022-05-27 09:23:47 +0200

    funzione vecchia

--- mqtt_app/web/main.js
+++ mqtt_app/web/main.js
@@ -1,56 +1,39 @@
-export const speedR = document.getElementById("speedRange");
-export const cameraR = document.getElementById("cameraRange");
-export const steeringR = document.getElementById("steeringRange");
+const speedR = document.getElementById("speedRange");
+const cameraR = document.getElementById("cameraRange");
+const steeringR = document.getElementById("steeringRange");
 const stop = document.getElementById("stop");
 let outSpeed = document.getElementById("outSpeed");
 let outCamera = document.getElementById("outCamera");
 let outSteering = document.getElementById("outSteering");
 
 
-//1 --> speed
-//2 --> steering
-//3 --> camera
-export function changeValue_Text(choice, value) {
-  switch (choice) {
-    case 1:
-      // --- SPEED ---
-      eel.changeSpeed(speed = speedR.value)
-      outSpeed.innerText = speedR.value
-      break;
-    case 2:
-      // --- STEERING ---
-      eel.changeSteering(steering = steeringR.value)
-      outSteering.innerText = steeringR.value
-      break;
-    default:
-      // --- CAMERA ---
-      eel.changeCamera(camera = cameraR.value)
-      outCamera.innerText = cameraR.value
-      break;
-  }
-}
 
 function stopEverything() {
   speedR.value = 0;
   steeringR.value = 0;
   cameraR.value = 0;
-  changeValue_Text(1, speedR.value)
+  eel.changeSpeed(speed = speedR.value)
+  outSpeed.innerText = speedR.value
   changeValue_Text(2, steeringR.value)
   changeValue_Text(3, cameraR.value)
 }
 speedR.addEventListener("change", () => {
-  changeValue_Text(1, speedR.value)
+  console.log("ciao")
+  eel.changeSpeed(speed = speedR.value)
+  outSpeed.innerText = speedR.value
 });
 cameraR.addEventListener("change", () => {
-  changeValue_Text(2, steeringR.value)
+  eel.changeCamera(camera = cameraR.value)
+  outCamera.innerText = cameraR.value
 });
 steeringR.addEventListener("change", () => {
-  changeValue_Text(3, cameraR.value)
+  eel.changeSteering(steering = steeringR.value)
+  outSteering.innerText = steeringR.value
 });
 
-stop.addEventListener("click", () => {
-  stopEverything()
-})
+// stop.addEventListener("click", () => {
+//   stopEverything()
+// })
 
 eel.expose(prompt_alerts);
 function prompt_alerts(description) {
@@ -64,26 +47,31 @@ document.addEventListener('keydown', (event) => {
 
   if (event.key === 'ArrowUp') {
     speedR.value = speed_val + 1
-    changeValue_Text(1, speedR.value)
+    eel.changeSpeed(speed = speedR.value)
+    outSpeed.innerText = speedR.value
   }
   else if (event.key === 'ArrowDown') {
     speedR.value = speed_val - 1
-    changeValue_Text(1, speedR.value)
+    eel.changeSpeed(speed = speedR.value)
+    outSpeed.innerText = speedR.value
   }
   else if (event.key === 'ArrowLeft') {
     steeringR.value = steering_val - 1
-    changeValue_Text(2, steeringR.value)
+    eel.changeSteering(steering = steeringR.value)
+    outSteering.innerText = steeringR.value
   }
   else if (event.key === 'ArrowRight') {
     steeringR.value = steering_val + 1
-    changeValue_Text(2, steeringR.value)
+    eel.changeSteering(steering = steeringR.value)
+    outSteering.innerText = steeringR.value
 
   }
   else if (event.code === 'Space') {
     // console.log("ciao")
     // stopEverything()
     steeringR.value = 0
-    changeValue_Text(2, steeringR.value)
+    eel.changeSteering(steering = steeringR.value)
+    outSteering.innerText = steeringR.value
   }
 
   outSpeed.innerText = speedR.value
@@ -107,25 +95,31 @@ window.addEventListener("gamepaddisconnected", e => {
   cancelRequestAnimationFrame(start);
   gamePad = null;
 });
-while (true) {
-  console.log("-" + gamePad.buttons)
-}
-console.log("CASDASD  ")
+
 function gameLoop() {
-  console.log("asdasdasdasdasd")
   if (!gamepad) {
     return;
   }
   if (gamePad.buttons[0] == 1) {
-    console.log("ADADAS")
-    changeValue_Text(2, 0) //--> STEERING
-    changeValue_Text(1, 0) //--> SPEED
+    speedR.value = 0;
+    steeringR.value = 0;
+    eel.changeSteering(steering = steeringR.value)
+    outSteering.innerText = steeringR.value
+    eel.changeSpeed(speed = speedR.value)
+    outSpeed.innerText = speedR.value
   }
 
   console.log(gamePad.buttons.axes[2] * 5)
 
-  changeValue_Text(2, gamePad.buttons.axes[2] * 5) //--> STEERING
-  changeValue_Text(1, gamePad.buttons.axes[3] * 5) //--> SPEED
-  changeValue_Text(3, gamePad.buttons.axes[0] * 5) //--> CAMERA
+  speedR.value = gamePad.buttons.axes[3] * 5;
+  steeringR.value = gamePad.buttons.axes[2] * 5;
+  cameraR.value = gamePad.buttons.axes[0] * 5;
+  eel.changeSteering(steering = steeringR.value)
+  outSteering.innerText = steeringR.value
+  eel.changeSpeed(speed = speedR.value)
+  outSpeed.innerText = speedR.value
+  eel.changeSteering(steering = steeringR.value)
+  outSteering.innerText = steeringR.value
+
   start = requestAnimationFrame(gameLoop);
 }
