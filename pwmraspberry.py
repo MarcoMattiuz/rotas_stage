4e62cf12f4611e1dc6842e568c424aa2b59c73e9
raspberry <marccs35@gmail.com>
2022-06-06 15:34:39 +0200

    pwm che funziona

--- pwmraspberry.py
+++ pwmraspberry.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+servoPIN = 11
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
@@ -8,17 +10,42 @@ GPIO.setup(31, GPIO.OUT)
 GPIO.setup(32, GPIO.OUT)
 GPIO.setup(12, GPIO.OUT)
 GPIO.setup(35, GPIO.OUT)
+GPIO.setup(servoPIN, GPIO.OUT)
 
-freq = 500
+freq = 50
 left1 = GPIO.PWM(31, freq)
 left2 = GPIO.PWM(32, freq)
 right1 = GPIO.PWM(12, freq)
 right2 = GPIO.PWM(35, freq)
+pwmS = GPIO.PWM(servoPIN, 50)
 
 left1.start(0)
 left2.start(0)
 right1.start(0)
 right2.start(0)
+pwmS.start(7)
+sleep(0.1)
+pwmS.ChangeDutyCycle(0)
+
+
+def map(x, in_min, in_max, out_min, out_max):
+    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+
+
+def set_camera(angle):
+    angle = int(angle)
+    if angle>=0 and angle<=5:
+        angle = 90 + (angle*10)
+    else:
+        print("Valori accettati da 0 a 5")
+        exit()
+    duty = angle / 18 + 2
+    print(duty)
+    GPIO.output(servoPIN, True)
+    pwmS.ChangeDutyCycle(duty)
+    sleep(0.3)
+    GPIO.output(servoPIN, False)
+    pwmS.ChangeDutyCycle(0)
 
 
 def stop_pwm():
@@ -26,12 +53,8 @@ def stop_pwm():
     left2.stop()
     right1.stop()
     right2.stop()
-
+    pwmS.stop()
     GPIO.cleanup()
-
-
-def map(x, in_min, in_max, out_min, out_max):
-    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
 
 def traz(velocita, sterzo):
@@ -71,3 +94,4 @@ def traz(velocita, sterzo):
 # sleep(5)
 
 # stop_pwm()
+
